c18f98900dcaaa9e88718c7aaa22cf131668b5bf
artur shyshko <arturshyshko@gmail.com>
2018-04-08 17:23:07 +0300

    registered dashboard models to admin app

--- server/powercor_dashboard/dashboard/admin.py
+++ server/powercor_dashboard/dashboard/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.client import Client
+from .models.manager import Manager
+from .models.discipline import Discipline
+from .models.project import Project
+
+from .models.choices import *
+
+
+admin.site.register(Client)
+admin.site.register(Manager)
+admin.site.register(Discipline)
+admin.site.register(Project)
+
+admin.site.register(BusinessImportanceChoice)
+admin.site.register(ResourcesChoice)
+admin.site.register(StageChoice)
+admin.site.register(StatusChoice)
