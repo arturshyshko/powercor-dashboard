9a2fde661e38b4a5f65eff0090519b5bd9b52478
artur shyshko <arturshyshko@gmail.com>
2018-04-14 19:14:52 +0300

    setup get api requests for all models vewsets

--- client/web/src/constants/apiUrls.js
+++ client/web/src/constants/apiUrls.js
@@ -1,1 +1,8 @@
 export const API_PROJECTS = '/api/projects/'
+export const API_DISCIPLINES = '/api/disciplines/'
+export const API_MANAGERS = '/api/managers/'
+export const API_CLIENTS = '/api/clients/'
+export const API_IMPORTANCES = '/api/importances/'
+export const API_RESOURCES = '/api/resources/'
+export const API_STAGES = '/api/stages/'
+export const API_STATUSES = '/api/statuses/'
--- client/web/src/services/apiAccess.js
+++ client/web/src/services/apiAccess.js
@@ -1,14 +1,48 @@
-import axios from 'axios'
-import { API_PROJECTS } from '../constants/apiUrls'
+import axios from './httpClient'
+import * as apiUrls from '../constants/apiUrls'
 
 
-export const fetchProjectsData = (cb) => {
+let fetchData = (cb, url) => {
     axios({
         method: 'GET',
-        url: API_PROJECTS,
+        url: url,
     }).then(response => {
         cb(response.data)
     }).catch(error => {
         console.log(error)
     })
 }
+
+export const fetchProjectsData = (cb) => {
+    fetchData(cb, apiUrls.API_PROJECTS)
+}
+
+export const fetchDisciplinesData = (cb) => {
+    fetchData(cb, apiUrls.API_DISCIPLINES)
+}
+
+export const fetchManagersData = (cb) => {
+    fetchData(cb, apiUrls.API_MANAGERS)
+}
+
+export const fetchClientsData = (cb) => {
+    fetchData(cb, apiUrls.API_CLIENTS)
+}
+
+export const fetchImportancesData = (cb) => {
+    fetchData(cb, apiUrls.API_IMPORTANCES)
+}
+
+export const fetchResourcesData = (cb) => {
+    fetchData(cb, apiUrls.API_RESOURCES)
+}
+
+export const fetchPStagesData = (cb) => {
+    fetchData(cb, apiUrls.API_STAGES)
+}
+
+export const fetchStatusesData = (cb) => {
+    fetchData(cb, apiUrls.API_STATUSES)
+}
+
+
