b66e63e541158f6a14ea9c566e03e9d3653680f1
artur shyshko <arturshyshko@gmail.com>
2018-05-05 16:04:03 +0300

    added api endpoint for getting all disciplines names

--- server/powercor_dashboard/dashboard/views/discipline.py
+++ server/powercor_dashboard/dashboard/views/discipline.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 from dashboard.models.discipline import Discipline
 from dashboard.serializers.discipline import DisciplineSerializer
@@ -8,3 +10,11 @@ from dashboard.serializers.discipline import DisciplineSerializer
 class DisciplineViewSet(viewsets.ModelViewSet):
     queryset = Discipline.objects.all()
     serializer_class = DisciplineSerializer
+
+
+@api_view(['GET'])
+def discipline_names(request):
+    queryset = Discipline.objects.all()
+    return Response(set(list(map(lambda discipline: discipline.get_name_display(), queryset))))
+
+
