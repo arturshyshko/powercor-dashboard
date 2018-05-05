8d3068f79b3a32706899d517ba290f0ed4d4d3c2
artur shyshko <arturshyshko@gmail.com>
2018-05-05 14:34:41 +0300

    action type as constants, rename actions for projects

--- client/web/src/actions/projects.js
+++ client/web/src/actions/projects.js
@@ -1,8 +1,16 @@
 export const GET_PROJECTS = 'GET_PROJECTS'
+export const UPDATE_PROJECT = 'UPDATE_PROJECT'
 
-export const fetchProjects = (data) => {
+export const getProjects = (data) => {
     return{
     type: GET_PROJECTS,
     payload: data,
+    }
+}
+
+export const updateProject = (data) => {
+    return {
+        type: UPDATE_PROJECT,
+        payload: data
     }
 }
