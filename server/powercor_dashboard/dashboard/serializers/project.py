e1cf4b6e9ab692eb4ea306b7380a0e3dcaca1d7b
artur shyshko <arturshyshko@gmail.com>
2018-07-14 23:38:49 +0300

    start setup for update in project serializer

--- server/powercor_dashboard/dashboard/serializers/project.py
+++ server/powercor_dashboard/dashboard/serializers/project.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from dashboard.models.project import Project
+from dashboard.models.client import Client
 
-from dashboard.serializers.discipline import DisciplineSerializer
+from dashboard.serializers.discipline import DisciplineSerializer, DisciplineInListSerializer
+from dashboard.serializers.manager import ManagerSerializer
+from dashboard.serializers.client import ClientSerializer
+from dashboard.serializers.choices import BusinessImportanceChoiceSerializer, ResourcesChoiceSerializer, \
+    StageChoiceSerializer, StatusChoiceSerializer
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    disciplines = DisciplineSerializer(many=True)
+
+class ProjectInListSerializer(serializers.ModelSerializer):
+    disciplines = DisciplineInListSerializer(many=True)
 
     class Meta:
         model = Project
@@ -18,5 +24,37 @@ class ProjectSerializer(serializers.ModelSerializer):
             'disciplines',
             'business_importance',
             'priority',
-            )
+        )
         read_only_fields = ('priority',)
+
+
+class ProjectSerializer(ProjectInListSerializer):
+    disciplines = DisciplineSerializer(many=True, partial=True)
+
+    class Meta(ProjectInListSerializer.Meta):
+        pass
+
+    def update(self, instance, validated_data):
+        disciplines_data = validated_data.pop('disciplines', None)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.comment = validated_data.get('comment', instance.comment)
+
+        client = validated_data.get('client')
+        if client:
+            instance.client = client
+
+        manager = validated_data.get('manager')
+        if manager:
+            instance.manager = manager
+
+        business_importance = validated_data.get('business_importance')
+        if business_importance:
+            instance.business_importance = business_importance
+
+        if disciplines_data:
+            print(disciplines_data)
+
+        instance.save()
+
+        return instance
