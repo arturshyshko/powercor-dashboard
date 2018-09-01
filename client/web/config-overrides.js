6ad8af74d436b1d86ca13f5484ede6c59dc2f040
Artur Shyshko <arturshyshko@gmail.com>
2018-09-02 00:18:53 +0300

    setup usage of local modules for client app

--- client/web/config-overrides.js
+++ client/web/config-overrides.js
@@ -1,7 +1,21 @@
 const rewireMobX = require('react-app-rewire-mobx')
+const path = require('path');
 
 
 module.exports = function override(config, env) {
-	config = rewireMobX(config, env)
-	return config
+	let appConfig = rewireMobX(config, env)
+	// Add local modules as aliases
+	// After adding new alias you have to restart application
+	var localModules = {
+		'@actions': path.resolve(__dirname, 'src', 'actions'),
+		'@components': path.resolve(__dirname, 'src', 'components'),
+		'@constants': path.resolve(__dirname, 'src', 'constants'),
+		'@services': path.resolve(__dirname, 'src', 'services'),
+		'@api': path.resolve(__dirname, 'src', 'api'),
+	}
+
+	// Add new alises to webpack config
+	appConfig.resolve.alias = Object.assign({}, config.resolve.alias, localModules)
+
+	return appConfig
 }
