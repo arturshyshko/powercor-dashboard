766a355cff47320ae6a4eb10476712ea757c48a6
Oleh Zorenko <lovemybeard@gmail.com>
2018-09-01 16:56:44 +0300

    add str to approved variation model

--- server/powercor_dashboard/dashboard/models/approved_variation.py
+++ server/powercor_dashboard/dashboard/models/approved_variation.py
@@ -7,3 +7,6 @@ class ApprovedVariation(models.Model):
     comment = models.TextField(blank=True, null=True)
     actual_cost = models.DecimalField(max_digits=14, decimal_places=2, blank=True, null=True)
     discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE, verbose_name='approved_variations')
+
+    def __str__(self):
+        return f'{self.discipline} - {self.id} - {self.actual_cost}'
