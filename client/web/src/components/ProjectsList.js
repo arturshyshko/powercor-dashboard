9b5c1d6f24bc8e614011a34a728c6cb2cb2b85b6
Oleh Zorenko <lovemybeard@gmail.com>
2018-09-02 14:17:27 +0300

    Remove upcoming milestones row and make disciplines use bold font

--- client/web/src/components/ProjectsList.js
+++ client/web/src/components/ProjectsList.js
@@ -29,21 +29,19 @@ export class ProjectsList extends React.Component {
     render() {
         return (
             <div>
-                <table className="table table-bordered table-hover" style={{textAlign: 'center', marginTop: '20px'}}>
+                <table className="table table-bordered table-hover"
+                       style={{textAlign: 'center', marginTop: '20px'}}>
                     <thead style={{textAlign: 'center'}}>
                         <tr>
                             <th rowSpan="2" style={{verticalAlign: 'middle'}}>Name</th>
                             <th rowSpan="2" style={{verticalAlign: 'middle'}}>Manager</th>
-                            <th rowSpan="2" style={{verticalAlign: 'middle'}}>Comments</th>
-                            <th colSpan="10" style={{verticalAlign: 'middle'}}>Upcoming milestones</th>
-                            <th rowSpan="2" style={{verticalAlign: 'middle'}}>Priority</th>
-                        </tr>
-                        <tr>
                             {
                                 this.props.disciplineNames.map((discipline, i) => (
-                                    <td key={i} colSpan="2">{discipline[1]}</td>
+                                    <th key={i} colSpan="2" style={{verticalAlign: 'middle'}}>{discipline[1]}</th>
                                 ))
                             }
+                            <th rowSpan="2" style={{verticalAlign: 'middle'}}>Priority</th>
+                            <th rowSpan="2" style={{verticalAlign: 'middle'}}>Comments</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -52,17 +50,17 @@ export class ProjectsList extends React.Component {
                                 <tr key={i} onClick={this.editProject} project={project.network} >
                                     <td>{project.network + ' ' + project.name}</td>
                                     <td>{this.props.managers.find(man => man.id === project.manager).name}</td>
-                                    <td>{project.comment}</td>
                                     {
                                         this.props.disciplineNames.map((discipline, i) => {
                                             let a = project.disciplines.find((disc) => (disc['name'] === discipline[0]))
                                             return [
                                                 <td key={i}>{a ? a['stage'] : ''}</td>,
-                                                <td key={i + 5}>{a ? a['due_date'] : ''}</td>
+                                                <td key={i + 5}>{a ? a['dueDate'] : ''}</td>
                                             ]
                                         })
                                     }
                                     <td>{project.priority}</td>
+                                    <td>{project.comment}</td>
                                 </tr>
                                 ))
                         }
