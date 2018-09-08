7569168b6829f5f2b8a2c2300606267bb3557d4d
Artur Shyshko <arturshyshko@gmail.com>
2018-09-08 21:33:04 +0300

    WIP

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -2,14 +2,49 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
-import { DASHBOARD } from './constants/clientUrls'
-import Dashboard from './containers/Dashboard'
+import { fetchData } from '@services/apiAccess'
+import { configureStore } from '@store'
+import store from '@store'
+import { API_STORE } from '@constants/apiUrls'
+
+import { DASHBOARD } from '@constants/clientUrls'
+import { TestView } from '@components/testMobX'
+import { Provider } from 'mobx-react'
+
+import {fetchManagersData} from '@api/managers'
+import {fetchProjectsData} from '@api/projects'
+import {fetchClientsData} from '@api/clients'
+import {fetchDisciplinesData} from '@api/disciplines'
+import {fetchImportancesData, fetchResourcesData, fetchStagesData, fetchStatusesData} from '@api/choices'
+import {fetchVariationsData} from '@api/approvedVariations'
+import {setManagers} from '@actions/manager'
+import {setProjects} from '@actions/project'
+import {setDisciplines} from '@actions/discipline'
+import {setClients} from '@actions/client'
+import {setImportanceChoices, setResourcesChoices, setStageChoices, setStatusChoices} from '@actions/choice'
+import {setVariations} from '@actions/approvedVariation'
 
 
-const App = () => (
-    <Switch>
-        <Route exact path={DASHBOARD} component={Dashboard} />
-    </Switch>
-)
+class App extends React.Component {
+    componentDidMount() {
+        fetchManagersData(setManagers)
+        fetchProjectsData(setProjects)
+        fetchClientsData(setClients)
+        fetchDisciplinesData(setDisciplines)
+        fetchImportancesData(setImportanceChoices)
+        fetchResourcesData(setResourcesChoices)
+        fetchStatusesData(setStatusChoices)
+        fetchStagesData(setStageChoices)
+        fetchVariationsData(setVariations)
+    }
+
+    render() {
+        return (
+            <Provider store={store} >
+                <div>testing MobX in progress</div>
+            </Provider>
+        )
+    }
+}
 
 export default App
--- client/web/src/index.js
+++ client/web/src/index.js
@@ -4,16 +4,13 @@ import App from './App'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { store } from './store';
-
 
 import { BrowserRouter } from 'react-router-dom'
 
+
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root'));
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
