5420e68522af9296d69f49942be1538a11e806f8
Artur Shyshko <arturshyshko@gmail.com>
2018-09-23 20:18:14 +0300

    small fixes + styling

--- client/web/src/index.js
+++ client/web/src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-table/react-table.css'
 import './index.css'
 import App from './App'
 
@@ -8,10 +9,11 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'mobx-react'
 import store from '@store'
+import {tracker} from '@services/apiAccess'
 
 
 ReactDOM.render(
-    <Provider store={store} >
+    <Provider store={store} tracker={tracker} >
         <BrowserRouter>
             <App />
         </BrowserRouter>
