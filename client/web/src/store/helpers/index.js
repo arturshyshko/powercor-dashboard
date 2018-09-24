ccb00709cfcbe73431c2e34a02a1e43baf934f69
Artur Shyshko <arturshyshko@gmail.com>
2018-09-25 01:02:48 +0300

    fix base functions creation for stores

--- client/web/src/store/helpers/index.js
+++ client/web/src/store/helpers/index.js
@@ -2,9 +2,14 @@ import { types } from 'mobx-state-tree'
 import { addObjectToArray, addObjectsToArray, setObjectToArray, setObjectsToArray } from '@store/helpers/baseActions'
 
 
-const createFunctionName = (prefix, collection, isMultiple='false') => (
-    prefix + collection[0].toUpperCase() + (isMultiple ? collection.slice(1, -1) : collection.slice(1))
-)
+const createFunctionName = (prefix, collection, isMultiple=false) => {
+    let result = prefix + collection[0].toUpperCase()
+    if (isMultiple === false) {
+        return result + collection.slice(1, -1)
+    } else {
+        return result + collection.slice(1)
+    }
+}
 
 const createBaseActions = (collection, findField='id') => (
     types.model({}).actions(self => ({
@@ -25,7 +30,7 @@ const createBaseActions = (collection, findField='id') => (
         },
 
         // Create function with 'setObjects' pattern
-        [createFunctionName('set', collection, false)](data) {
+        [createFunctionName('set', collection, true)](data) {
             return setObjectsToArray(data, self[collection], findField)
         },
     }))
