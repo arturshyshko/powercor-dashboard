078a57f2c8c89dd58e03ae0a9c24c14303f282b4
Artur Shyshko <arturshyshko@gmail.com>
2019-04-04 15:45:45 +0300

    Changes to MST store - Replace asyncReference with types.reference everywhere - Move disciplines and approvedVariations to parent model as arrays

--- client/web/src/store/project.js
+++ client/web/src/store/project.js
@@ -1,10 +1,10 @@
-import { types, getRoot } from 'mobx-state-tree'
+import { types } from 'mobx-state-tree'
 
 import createAllActions from '@store/helpers'
-import asyncReference from '@store/helpers/asyncIdentifier'
 
 import { API_PROJECTS } from '@constants/apiUrls'
 
+import { Discipline } from '@store/discipline'
 import { Manager } from '@store/manager'
 import { Client } from '@store/client'
 import { BusinessImportanceChoice } from '@store/choice'
@@ -13,47 +13,14 @@ import { BusinessImportanceChoice } from '@store/choice'
 export const Project = types.model('Project', {
     network: types.identifierNumber,
     name: types.string,
-    manager: asyncReference(Manager),
-    client: asyncReference(Client),
+    manager: types.reference(Manager),
+    client: types.reference(Client),
     comment: types.optional(types.string, ''),
-    businessImportance: asyncReference(BusinessImportanceChoice, 'getOrLoadChoice'),
+    businessImportance: types.reference(BusinessImportanceChoice),
     priority: types.maybeNull(types.number),
     status: types.string,
-}).views(self => ({
-
-    get disciplines() {
-        return getRoot(self).disciplineStore.getProjectDisciplines(self).reduce((result, disc) => {
-            result[disc.name] = disc
-
-            return result
-        }, {})
-    },
-
-})).actions(self => ({
-
-    setManager(manager) {
-        self.manager = manager;
-    },
-    setName(name) {
-        self.name = name
-    },
-    setNetwork(network) {
-        self.network = network
-    },
-    setClient(client) {
-        self.client = client
-    },
-    setComment(comment) {
-        self.comment = comment
-    },
-    setBusinessImportance(importance) {
-        self.businessImportance = importance
-    },
-    setStatus(status) {
-        self.status = status
-    },
-
-}))
+    disciplines: types.array(Discipline),
+})
 
 
 const ProjectStore = types.compose(
