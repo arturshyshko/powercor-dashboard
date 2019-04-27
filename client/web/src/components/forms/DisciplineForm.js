27b375178a203225a15afce837e7ede13fa73167
Artur Shyshko <arturshyshko@gmail.com>
2019-04-27 14:44:39 +0300

    Base discipline and variation forms

--- client/web/src/components/forms/DisciplineForm.js
+++ client/web/src/components/forms/DisciplineForm.js
@@ -1,14 +1,78 @@
 import React, { Fragment } from 'react'
+import { observer } from 'mobx-react'
 
 import { InputText, InputTextArea, InputSelect } from '@components/helpers/Form'
+import ApprovedVariationForm from './ApprovedVariationForm'
 
 
+@observer
 class DisciplineForm extends React.Component {
     render() {
-        const { discipline, handleInputChange } = this.props
+        const { discipline, handleInputChange, handleVariationChange,
+            disciplineNames, stages, resources, statuses} = this.props
+
         return (
             <Fragment>
-                <div>lol</div>
+                <div className="form-row">
+                    <InputSelect
+                        label="Name: "
+                        control="name"
+                        colWidth="2"
+                        className="form-control"
+                        value={discipline.name}
+                        options={disciplineNames}
+                        handleChange={(e) => handleInputChange(e, discipline.id)}
+                    />
+                    <InputSelect
+                        label="Stage: "
+                        control="stage"
+                        colWidth="2"
+                        className="form-control"
+                        value={discipline.stage}
+                        options={stages}
+                        handleChange={e => handleInputChange(e, discipline.id)}
+                    />
+                    <InputSelect
+                        label="Resources: "
+                        control="resources"
+                        colWidth="2"
+                        className="form-control"
+                        value={discipline.resources}
+                        options={resources}
+                        handleChange={e => handleInputChange(e, discipline.id)}
+                    />
+                    <InputSelect
+                        label="Status: "
+                        control="status"
+                        colWidth="2"
+                        className="form-control"
+                        value={discipline.status}
+                        options={statuses}
+                        handleChange={e => handleInputChange(e, discipline.id)}
+                    />
+                    <InputText
+                        label="Budget: "
+                        control="budget"
+                        colWidth="2"
+                        className="form-control"
+                        value={discipline.budget}
+                        handleChange={e => handleInputChange(e, discipline.id)}
+                    />
+                    <InputText
+                        label="Actual Cost: "
+                        control="actualCost"
+                        colWidth="2"
+                        className="form-control"
+                        value={discipline.actualCost}
+                        handleChange={e => handleInputChange(e, discipline.id)}
+                    />
+                </div>
+                {discipline.approvedVariations.map(variation => (
+                    <ApprovedVariationForm
+                        variation={variation}
+                        handleInputChange={handleVariationChange}
+                    />
+                ))}
             </Fragment>
         )
     }
